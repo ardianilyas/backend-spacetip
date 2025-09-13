4dd07fc784dcea64730d03589c9a037045500b7d
ardianilyas <ardianilyas@gmail.com>
2025-09-13 21:52:04 +0700

    feat: update auth middleware request

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,21 +1,21 @@
-import type { NextFunction, Request, RequestHandler, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { verifyAccessToken } from "../utils/jwt.js";
 
 export interface AuthRequest extends Request {
     user: { userId: string, role: string };
 }
 
-export const requireAuth: RequestHandler = (req, res, next) => {
+export function requireAuth(req: Request, res: Response, next: NextFunction): void {
     try {
       const access = req.cookies?.accessToken;
       if (!access) {
-        return res.status(401).json({ success: false, message: "Unauthorized" });
+        res.status(401).json({ success: false, message: "Unauthorized" });
       }
   
       const payload = verifyAccessToken(access);
       (req as AuthRequest).user = { userId: payload.userId, role: payload.role };
       next();
     } catch {
-      return res.status(401).json({ success: false, message: "Invalid token" });
+        res.status(401).json({ success: false, message: "Invalid token" });
     }
   };
