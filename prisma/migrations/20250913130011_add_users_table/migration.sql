f36367edaea5905fca3645e8d5f48de01801553b
ardianilyas <ardianilyas@gmail.com>
2025-09-13 20:05:13 +0700

    feat: add prisma schema

---
+++ prisma/migrations/20250913130011_add_users_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "public"."RoleUser" AS ENUM ('USER', 'ADMIN', 'SUPERADMIN', 'CREATOR');
+
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."RoleUser" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
