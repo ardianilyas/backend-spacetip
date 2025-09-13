4dea83079375de9affad6c87f38b8ebc0bfd62cf
ardianilyas <ardianilyas@gmail.com>
2025-09-14 01:35:37 +0700

    feat: update auth route, grouping require auth

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller.js";
+import { requireAuth } from "../../middlewares/auth.js";
 
 const router = Router();
 const controller = new AuthController();
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
-router.post("/refresh", controller.refresh);
-router.post("/logout", controller.logout);
+
+router.use(requireAuth, Router()
+    .post("/refresh", controller.refresh)
+    .post("/logout", controller.logout)
+);
 
 export default router;
