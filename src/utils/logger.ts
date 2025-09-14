703b5afab4bb50e32f9fe092bc75a829e7edfb20
ardianilyas <ardianilyas@gmail.com>
2025-09-14 20:35:52 +0700

    feat: update logger add errors and server log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,5 +2,7 @@ import { createLogger } from "../config/logger";
 
 export const logger = {
   app: createLogger("app.log"),
+  error: createLogger("error.log"),
+  server: createLogger("server.log"),
   job: createLogger("job.log"),
 };
