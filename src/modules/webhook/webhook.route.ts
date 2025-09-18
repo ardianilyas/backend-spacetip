51dad971e6a3d3c0523a84430fb1c568fd47fdd6
ardianilyas <ardianilyas@gmail.com>
2025-09-18 15:35:53 +0700

    feat: update webhook route to be using service repository

--- src/modules/webhook/webhook.route.ts
+++ src/modules/webhook/webhook.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { WebhookController } from "./webhook.controller";
+import { WebhookRepository } from "./webhook.repository";
+import { WebhookService } from "./webhook.service";
 
 const router = Router();
-const controller = new WebhookController();
+
+const webhookRepo = new WebhookRepository();
+const webhookService = new WebhookService(webhookRepo); 
+const controller = new WebhookController(webhookService);
 
 router.post("/payments/webhook", controller.webhook);
 
