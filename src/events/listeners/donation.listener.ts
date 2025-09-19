f82326562146388d78adb0a2859dc56628872503
ardianilyas <ardianilyas@gmail.com>
2025-09-19 23:27:43 +0700

    feat: change creatorToken

--- src/events/listeners/donation.listener.ts
+++ src/events/listeners/donation.listener.ts
@@ -5,7 +5,7 @@ import { DonationPaidPayload } from "../types/donation.type";
 appEventEmitter.on("donation.paid", (payload: DonationPaidPayload) => {
     const io = getIO();
 
-    io.to(`creator_${payload.creatorId}`).emit("donation_message", {
+    io.to(`creator_${payload.creatorToken}`).emit("donation_message", {
         donationId: payload.donationId,
         amount: payload.amount,
         message: payload.message,
