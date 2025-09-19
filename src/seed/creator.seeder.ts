7a305d9304309c21269724d8c17139c9cd2bd8cd
ardianilyas <ardianilyas@gmail.com>
2025-09-19 23:26:00 +0700

    feat: update creator seeder

--- src/seed/creator.seeder.ts
+++ src/seed/creator.seeder.ts
@@ -9,10 +9,23 @@ export async function creatorSeeder() {
 
     const selected = shuffled.slice(0, Math.floor(Math.random() * 2) + 3);
 
+    const developer = await prisma.user.findUnique({ where: { email: "ardian@developer.com" } });
+
+    await prisma.creator.create({
+        data: {
+            userId: developer!.id,
+            username: "ardianilyas",
+            token: crypto.randomBytes(8).toString("hex"),
+            bio: faker.person.bio(),
+        }
+    });
+
     for (const user of selected) {
         const token = crypto.randomBytes(8).toString("hex");
-        await prisma.creator.create({
-            data: {
+        await prisma.creator.upsert({
+            where: { userId: user.id },
+            update: {},
+            create: {
                 userId: user.id,
                 username: faker.internet.username(),
                 token,
