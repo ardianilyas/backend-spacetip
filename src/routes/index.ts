547430b46d178f5867dbd4f83685b48d0b73cfe1
ardianilyas <ardianilyas@gmail.com>
2025-09-20 21:10:18 +0700

    feat: update index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoute from "../modules/auth/auth.route";
 import creatorRouter from "../modules/creator/creator.route";
 import donationRouter from "../modules/donation/donation.route";
 import webhookRouter from "../modules/webhook/webhook.route";
+import withdrawalRouter from "../modules/withdrawal/withdrawal.route";
 
 const router = Router();
 const apiRouter = router;
@@ -10,7 +11,8 @@ const apiRouter = router;
 apiRouter.use("/", webhookRouter);
 apiRouter.use("/auth", authRoute);
 apiRouter.use("/creators", creatorRouter);
-apiRouter.use("/", donationRouter);
+apiRouter.use("/donaio", donationRouter);
+apiRouter.use("/withdrawals", withdrawalRouter);
 
 router.use("/", apiRouter);
 
