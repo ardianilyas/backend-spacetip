6e5f577a85bccda13b700080759722d7bfba7662
ardianilyas <ardianilyas@gmail.com>
2025-09-20 17:28:07 +0700

    feat: update creator repository to be select only username, bio, and isVerified

--- src/modules/creator/creator.repository.ts
+++ src/modules/creator/creator.repository.ts
@@ -1,19 +1,20 @@
 import { Creator } from "../../../prisma/generated/prisma";
 import { prisma } from "../../config/prisma";
 import { CreateCreatorSchema } from "./creator.schema";
+import { CreatorDataType } from "./creator.type";
 
 export class CreatorRepository {
 
-    async getAllCreators(): Promise<Creator[]> {
-        return prisma.creator.findMany();
+    async getAllCreators(): Promise<CreatorDataType[]> {
+        return prisma.creator.findMany({ select: { username: true, bio: true, isVerified: true } });
     }
 
     async createCreatorAccount(data: CreateCreatorSchema): Promise<Creator> {
         return prisma.creator.create({ data });
     }
 
-    async findCreatorByUsername(username: string): Promise<Creator | null> {
-        return prisma.creator.findUnique({ where: { username } });
+    async findCreatorByUsername(username: string): Promise<CreatorDataType | null> {
+        return prisma.creator.findUnique({ where: { username }, select: { username: true, bio: true, isVerified: true } });
     }
 
     async verifyCreator(userId: string): Promise<Creator> {
