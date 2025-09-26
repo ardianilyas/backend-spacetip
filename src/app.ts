e0fbaec2703da0bde0330a459788ca42b066f941
ardianilyas <ardianilyas@gmail.com>
2025-09-26 17:34:36 +0700

    feat: update cors origin

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
